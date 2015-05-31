3847cc956bb8dd2ef0a58dc4ed3e7fd2317dfd9d
Alex Goida <alex.goida@xtrmstep.ru>
2015-05-31 15:29:52 +0300

    Find substring project

---
+++ FindSubstring/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
